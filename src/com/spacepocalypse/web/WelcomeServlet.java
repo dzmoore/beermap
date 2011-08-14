b798f3c907d5af2ecd049517b243eba809c179f9
dylan <none@none>
2011-08-14 16:52:30 -0400

    Test commit. Removed single space.

--- src/com/spacepocalypse/web/WelcomeServlet.java
+++ src/com/spacepocalypse/web/WelcomeServlet.java
@@ -25,7 +25,7 @@ public class WelcomeServlet extends HttpServlet {
 
 		// Create output (the response):
 		out.println( "<html><head><title>BeerMap</title></head>" );
-		out.println( "<body>" );
+		out.println("<body>" );
 		out.println( "<h2>Welcome to <b>BeerMap</b></h2><br />");
 		out.println("<form name=\"beer_search_form\" action=\"beersearch\" method=\"get\">");
 		out.println("<input type=\"submit\" value=\"BeerSearch\" />");
