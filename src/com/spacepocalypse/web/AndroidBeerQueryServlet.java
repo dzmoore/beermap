a0cad8e61a31d094b688f42ae90a2028134ed1bf
dylan <none@none>
2011-09-03 13:21:17 -0400

    Added log4j support; added log message in AndroidBeerQueryServlet

--- src/com/spacepocalypse/web/AndroidBeerQueryServlet.java
+++ src/com/spacepocalypse/web/AndroidBeerQueryServlet.java
@@ -1,15 +1,15 @@
 package com.spacepocalypse.web;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -18,11 +18,38 @@ import com.spacepocalypse.pojo.MappedBeer;
 
 public class AndroidBeerQueryServlet extends HttpServlet {
 	private static final long serialVersionUID = -6893360066632654398L;
+	private Logger log4jLogger;
 
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		log4jLogger = Logger.getLogger(AndroidBeerQueryServlet.class);
+		log4jLogger.info("Initializing.");
+	}
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
+		StringBuilder sb = new StringBuilder();
+		boolean notFirst = false;
+		for (String key : req.getParameterMap().keySet()) {
+			if (notFirst) {
+				sb.append(", ");
+			}
+			sb.append(key);
+			sb.append("=[");
+			boolean innerNotFirst = false;
+			for (String value : req.getParameterMap().get(key)) {
+				if (innerNotFirst) {
+					sb.append(", ");
+				}
+				sb.append(value);
+				innerNotFirst = true;
+			}
+			sb.append("]");
+			notFirst = true;
+		}
+		log4jLogger.info("Request's parameter map=[" + sb.toString() + "]");
 		List<MappedBeer> beers = BeerSearchEngine.getInstance().doSearch(req.getParameterMap());
 		JSONArray jsonArr = new JSONArray();
 		for (MappedBeer ea : beers) {
